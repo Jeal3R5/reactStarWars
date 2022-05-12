df76df3fb9223463a1d66b51245bb5cf7fdde39d
Joan Renfroe <joan.renfroe@gmail.com>
2022-05-12 01:10:04 -0700

    more css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Body(props) {
   return (
-    <>
+    <div className="bodyDiv">
       {props.starship.map((ship, index) => {
         console.log(ship);
         return <div key={index}>{ship.name}</div>;
       })}
-    </>
+    </div>
   );
 }
 
