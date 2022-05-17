5164c438b979d9c8945198e2ba7edfcb4eb40062
Joan Renfroe <joan.renfroe@gmail.com>
2022-05-16 23:03:46 -0700

    add card flip css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
+// function Body(props) {
+//   return (
+//     <div className="bodyDiv">
+//       {props.starship.map((ship, index) => {
+//         console.log(ship);
+//         return (
+//           <div key={index} className="shipBox">
+//             {ship.name}
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+// export default Body;
+
+
 function Body(props) {
   return (
     <div className="bodyDiv">
@@ -7,7 +25,19 @@ function Body(props) {
         console.log(ship);
         return (
           <div key={index} className="shipBox">
-            {ship.name}
+            <div class="ship-card">
+              <div class="ship-card-inner">
+                <div class="ship-card-front">
+                  <p>{ship.name}</p>
+                </div>
+                <div class="ship-card-back">
+                  <div>{ship.model}</div>
+                  <br />
+                  <div>{ship.starship_url}</div>
+                  <a href="{ship.url}">For more details...</a>
+                </div>
+              </div>
+            </div>
           </div>
         );
       })}
